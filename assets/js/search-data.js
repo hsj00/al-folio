e448dd21d154d94bcfc8e73d23e6b86ac0b2d31c
hsj00 <54518893+hsj00@users.noreply.github.com>
2026-04-19 03:28:40 +0000

    Deploying to gh-pages from @ hsj00/hsj00.github.io@fef41645876d7c93cbd7dab2d812889af32ec40e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-bookshelf",
-          title: "bookshelf",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/books/";
-          },
-        },{id: "nav-blog",
+  },{id: "nav-blog",
           title: "blog",
           description: "",
           section: "Navigation",
@@ -30,9 +23,16 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
+        },{id: "nav-bookshelf",
+          title: "bookshelf",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/books/";
+          },
         },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Semiconductor process engineer specializing in metal ALD with 8+ years of experience across research, field engineering, and process development.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
